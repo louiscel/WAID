193e00e95768c6664d5ba33ddce90143323a2b76
Guillaume Conchon <guillaume.conchon@gmail.com>
2015-06-11 23:24:44 +0200

    first import

--- mobile/src/main/java/com/ca/wayd/ListActivity.java
+++ mobile/src/main/java/com/ca/wayd/ListActivity.java
@@ -2,23 +2,38 @@ package com.ca.wayd;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Chronometer;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ListActivity extends Activity {
 
     private ListView listView;
+    private TextView textView;
+    private Chronometer chrono;
+    private String currentActivity;
+    private long     startedTime = 0;
+    private Map<String, Long> timers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        timers = new HashMap<>();
+
         setContentView(R.layout.list_activity);
 
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.list);
+        textView = (TextView) findViewById(R.id.text);
+        chrono   = (Chronometer) findViewById(R.id.chronometer);
 
         // Defined Array values to show in ListView
         String[] values = new String[200];
@@ -44,12 +59,34 @@ public class ListActivity extends Activity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Récupération de la valeur cliquée
                 String  itemValue    = (String) listView.getItemAtPosition(position);
-
+                textView.setText(itemValue);
+                startChrono(itemValue);
                 // Affiche les infos de l'activité sélectionnée
-                Toast.makeText(getApplicationContext(),"Activité sélectionnée : " + position + "  Valeur : " + itemValue, Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(),"Activité sélectionnée : " + position + "  Valeur : " + itemValue, Toast.LENGTH_LONG).show();
 
             }
 
         });
+    }
+
+    private void startChrono(String activityId) {
+        // Récupération de l'heure courante
+        long time = SystemClock.elapsedRealtime();
+        // On sauvegarde le timer courant
+        if (currentActivity != null) {
+            long elapsed = time - chrono.getBase();
+            timers.put(currentActivity, elapsed);
+        }
+
+        // Récupération de l'ancien chrono si existant
+        Long lastTime = timers.get(activityId);
+        if (lastTime == null) {
+            lastTime = 0l;
+        }
+        currentActivity = activityId;
+
+        chrono.setBase(time - lastTime);
+        chrono.stop();
+        chrono.start();
     }
 }
